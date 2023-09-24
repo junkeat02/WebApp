96a150acf1c3c0fa6264f9cbaaeb2ab84edc32cd
junkeat02 <leejunk@student.usm.my>
2023-09-24 23:23:56 +0800

    first stage completion

--- form.py
+++ form.py
@@ -8,13 +8,24 @@ PASSENGERS = ["Lee", "Thomas", "Bin Tak", "Jin Er", "Han Lin"]
 class SignForm(FlaskForm):
     username = StringField("Username: ", validators=[DataRequired()])
     password = StringField("Password: ", validators=[DataRequired()])
+    submit = SubmitField()
 
 
 class CheckInForm(FlaskForm):
-    passengers = RadioField("Passengers", validators=[DataRequired()], choices=[x for x in PASSENGERS[:1]])
+    # passenger = RadioField("Passengers", validators=[DataRequired()])
+    passenger1 = RadioField("Passengers", validators=[DataRequired()],
+                            choices=[(1, PASSENGERS[0])])
+    passenger2 = RadioField("Passengers", validators=[DataRequired()],
+                            choices=[(2, PASSENGERS[1])])
+    passenger3 = RadioField("Passengers", validators=[DataRequired()],
+                            choices=[(3, PASSENGERS[2])])
+    passenger4 = RadioField("Passengers", validators=[DataRequired()],
+                            choices=[(4, PASSENGERS[3])])
+    passenger5 = RadioField("Passengers", validators=[DataRequired()],
+                            choices=[(5, PASSENGERS[4])])
+
     # passenger2 = RadioField(PASSENGERS[1], validators=[DataRequired()])
     # passenger3 = RadioField(PASSENGERS[2], validators=[DataRequired()])
     # passenger4 = RadioField(PASSENGERS[3], validators=[DataRequired()])
     # passenger5 = RadioField(PASSENGERS[4], validators=[DataRequired()])
     submit = SubmitField()
-
